738457a0968a77f8f9f7bba6977fcca54b1df28e
prashant2254 <91815061+prashant2254@users.noreply.github.com>
2023-11-30 14:31:21 +0530

    Add files via upload

---
+++ models/movie.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  content: { type: String, required: true, maxLength: 500 },
+  rating: { type: Number, required: true, min: 1, max: 5 },
+  author: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const movieSchema = new mongoose.Schema({
+  title: { type: String, required: true, maxLength: 255 },
+  description: { type: String, required: true, maxLength: 1000 },
+  genre: { type: String, required: true },
+  releaseYear: { type: Number, required: true, min: 1800, max: new Date().getFullYear() },
+  reviews: [reviewSchema],
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
